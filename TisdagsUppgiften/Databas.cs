9e73c273c06797c97a2edd37fb53eab945527ddc
Tove Seger <tove.seger@outlook.com>
2021-02-02 12:16:21 +0100

    Add project files.

---
+++ TisdagsUppgiften/Databas.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace TisdagsUppgiften
+{
+    class Databas
+    {
+        public static string connectionString { get; set; } = @"Data Source =.\SQLExpress; Integrated Security = true; database = {0}";
+
+        public static string databaseName { get; set; } = "Population";
+
+
+      /*  Skapa en metod som tar emot SQL string och parametrar och som exekverar din SQL kod och
+            returnerar ingenting(ExecuteNonQuery)
+            2. Skapa en metod som tar emot SQL string och parametrar och som exekverar din SQL kod och
+            returnerar en DataTable*/
+        public static long RunSQL(string s, string p)
+        {
+            string db = string.Format(connectionString, databaseName);
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand(s, conn);
+                cmd.Parameters.AddWithValue("@input", p);
+                long rows = cmd.ExecuteNonQuery();
+
+                return rows;
+                //conn.Close();
+            }
+        }
+
+        public static DataTable RunSQLDT(string s, string p)
+        {
+            string db = string.Format(connectionString, databaseName);
+            using (var conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand(s, conn);
+                cmd.Parameters.AddWithValue("@input", p);
+                long rows = cmd.ExecuteNonQuery();
+
+                DataTable dt = new DataTable();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(dt);
+                return dt;
+                //conn.Close();
+            }
+        }
+
+        public static void ShowTable(DataTable dtb)
+        {
+            foreach (DataRow row in dtb.Rows)
+            {
+                Console.WriteLine($"{row["id"]} {row["name"]} {row["age"]} ");
+            }
+        }
+    }
+}
---
+++ TisdagsUppgiften/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Data;
+
+namespace TisdagsUppgiften
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Insert SQL command:");
+            string command = Console.ReadLine();
+            Console.Write("Insert parameter:"); //behövs inte om man skriver allt i insert SQL command
+            string parameter = Console.ReadLine();
+            DataTable dtb = Databas.RunSQLDT(command, parameter);
+            Databas.ShowTable(dtb);
+            Console.ReadKey();
+        }
+
+
+    }
+}
